422af3ed75356f670e078eacd3738b509c785645
Alessandro Innocenti <alessandro.innocenti8@stud.unifi.it>
2024-05-14 10:32:18 +0200

    Code refactoring

--- confronto/main.py
+++ confronto/main.py
@@ -14,6 +14,26 @@ def randomword(length):
     return ''.join(random.choice(letters) for _ in range(length))
 
 
+def get_input_integer(text):
+    while True:
+        try:
+            r = int(input(text))
+            if r > 0:
+                return r
+            else:
+                print("Inserisci un numero intero positivo")
+        except ValueError:
+            print("Inserisci un numero intero valido")
+
+
+def graph_plot(y_axis, title):
+    plt.plot(y_axis)
+    plt.title(title)
+    plt.xlabel('Lunghezza in caratteri')
+    plt.ylabel('Tempo impiegato (secondi)')
+    plt.show()
+
+
 x_brute_force_axis = []
 x_recursive_axis = []
 x_recursive_memoization_axis = []
@@ -61,19 +81,20 @@ if auto_execution:
         y_bottom_up_axis.append(t1 - t0)
 else:
     different_nm = False
-    response = input("Vuoi mantenere le lunghezze delle stringhe uguali(m=n)?[Digitare [S] se si/altro o niente per mantenere la lunghezza minore fissa e variare la lunghezza dell'altra stringa]:")
+    response = input(
+        "Vuoi mantenere le lunghezze delle stringhe uguali(m=n)?[Digitare [S] se si/altro o niente per mantenere la lunghezza minore fissa e variare la lunghezza dell'altra stringa]:")
     if response == 'S':
         different_nm = True
 
     if response:
-        nfb = int(input(
-            "Inserisci il range della lunghezza delle stringhe per l'algoritmo Forza-Bruta (è fortemente consigliato inserire un valore 9<n<21): "))
-        nr = int(input(
-            "Inserisci il range della lunghezza delle stringhe per l'algoritmo Ricorsivo (è fortemente consigliato inserire un valore 9<n<15): "))
-        nrm = int(input(
-            "Inserisci il range della lunghezza delle stringhe per l'algoritmo Ricorsivo con memorizzazione (è fortemente consigliato inserire un valore 50<n<500): "))
-        nbu = int(input(
-            "Inserisci il range della lunghezza delle stringhe per l'algoritmo Bottom-Up (è fortemente consigliato inserire un valore 50<n<700): "))
+        nfb = get_input_integer(
+            "Inserisci il range della lunghezza delle stringhe per l'algoritmo Forza-Bruta (è fortemente consigliato inserire un valore 9<n<21): ")
+        nr = get_input_integer(
+            "Inserisci il range della lunghezza delle stringhe per l'algoritmo Ricorsivo (è fortemente consigliato inserire un valore 9<n<15): ")
+        nrm = get_input_integer(
+            "Inserisci il range della lunghezza delle stringhe per l'algoritmo Ricorsivo con memorizzazione (è fortemente consigliato inserire un valore 50<n<500): ")
+        nbu = get_input_integer(
+            "Inserisci il range della lunghezza delle stringhe per l'algoritmo Bottom-Up (è fortemente consigliato inserire un valore 50<n<700): ")
 
         for i in range(1, nfb + 1):
             x_brute_force_axis.append(i)
@@ -119,26 +140,29 @@ else:
             t1 = time()
             y_bottom_up_axis.append(t1 - t0)
     else:
-        fixed_mfb = int(input("Inserisci la lunghezza fissa della stringa più corta per l'algoritmo Forza-Bruta: "))
-        nfb = int(input(
-            "Inserisci il range della lunghezza della stringa per l'algoritmo Forza-Bruta (di default/inserimento valore erraro sarà maggiore del valore precedentemente inserito): "))
-        if nfb<=fixed_mfb:
-            nfb = fixed_mfb+1
-        fixed_mr = int(input("Inserisci la lunghezza fissa della stringa più corta per l'algoritmo Ricorsivo: "))
-        nr = int(input(
-            "Inserisci il range della lunghezza della stringa per l'algoritmo Ricorsivo (di default/inserimento valore erraro maggiore del valore precedentemente inserito): "))
-        if nr<=fixed_mr:
-            nr = fixed_mr+1
-        fixed_mrm = int(input("Inserisci la lunghezza fissa della stringa più corta per l'algoritmo Ricorsivo con memorizzazione: "))
-        nrm = int(input(
-            "Inserisci il range della lunghezza della stringa per l'algoritmo Ricorsivo con memorizzazione (di default/inserimento valore erraro maggiore del valore precedentemente inserito): "))
-        if nrm<=fixed_mrm:
-            nrm = fixed_mrm+1
-        fixed_mbu = int(input("Inserisci la lunghezza fissa della stringa più corta per l'algoritmo Bottom-Up: "))
-        nbu = int(input(
-            "Inserisci il range della lunghezza della stringa per l'algoritmo Bottom-Up (di default/inserimento valore erraro maggiore del valore precedentemente inserito): "))
-        if nbu<=fixed_mbu:
-            nbu = fixed_mbu+1
+        fixed_mfb = get_input_integer(
+            "Inserisci la lunghezza fissa della stringa più corta per l'algoritmo Forza-Bruta: ")
+        nfb = get_input_integer(
+            "Inserisci il range della lunghezza della stringa per l'algoritmo Forza-Bruta (di default/inserimento valore erraro sarà maggiore del valore precedentemente inserito): ")
+        if nfb <= fixed_mfb:
+            nfb = fixed_mfb + 1
+        fixed_mr = get_input_integer("Inserisci la lunghezza fissa della stringa più corta per l'algoritmo Ricorsivo: ")
+        nr = get_input_integer(
+            "Inserisci il range della lunghezza della stringa per l'algoritmo Ricorsivo (di default/inserimento valore erraro maggiore del valore precedentemente inserito): ")
+        if nr <= fixed_mr:
+            nr = fixed_mr + 1
+        fixed_mrm = get_input_integer(
+            "Inserisci la lunghezza fissa della stringa più corta per l'algoritmo Ricorsivo con memorizzazione: ")
+        nrm = get_input_integer(
+            "Inserisci il range della lunghezza della stringa per l'algoritmo Ricorsivo con memorizzazione (di default/inserimento valore erraro maggiore del valore precedentemente inserito): ")
+        if nrm <= fixed_mrm:
+            nrm = fixed_mrm + 1
+        fixed_mbu = get_input_integer(
+            "Inserisci la lunghezza fissa della stringa più corta per l'algoritmo Bottom-Up: ")
+        nbu = get_input_integer(
+            "Inserisci il range della lunghezza della stringa per l'algoritmo Bottom-Up (di default/inserimento valore erraro maggiore del valore precedentemente inserito): ")
+        if nbu <= fixed_mbu:
+            nbu = fixed_mbu + 1
 
         fixed_random_word = randomword(fixed_mfb)
         for i in range(nfb + 1):
@@ -184,7 +208,6 @@ else:
             t1 = time()
             y_bottom_up_axis.append(t1 - t0)
 
-
 plt.plot(x_brute_force_axis, y_brute_force_axis, label='Forza-Bruta')
 plt.plot(x_recursive_axis, y_recursive_axis, label='Ricorsivo')
 plt.plot(x_recursive_memoization_axis, y_recursive_memoization_axis, label='Ric. con memorizzazione')
@@ -195,26 +218,10 @@ plt.ylabel('Tempo impiegato (secondi)')
 plt.legend()
 plt.show()
 
-plt.plot(y_brute_force_axis)
-plt.title('Forza Bruta')
-plt.xlabel('Lunghezza stringhe (caratteri)')
-plt.ylabel('Tempo impiegato (secondi)')
-plt.show()
+graph_plot(y_brute_force_axis, 'Forza Bruta')
 
-plt.plot(y_recursive_axis)
-plt.title('Ricorsivo')
-plt.xlabel('Lunghezza stringhe (caratteri)')
-plt.ylabel('Tempo impiegato (secondi)')
-plt.show()
+graph_plot(y_recursive_axis, 'Ricorsivo')
 
-plt.plot(y_recursive_memoization_axis)
-plt.title('Ricorsivo con memorizzazione')
-plt.xlabel('Lunghezza stringhe (caratteri)')
-plt.ylabel('Tempo impiegato (secondi)')
-plt.show()
+graph_plot(y_recursive_memoization_axis, 'Ricorsivo con memorizzazione')
 
-plt.plot(y_bottom_up_axis)
-plt.title('Bottom Up')
-plt.xlabel('Lunghezza stringhe (caratteri)')
-plt.ylabel('Tempo impiegato (secondi)')
-plt.show()
+graph_plot(y_bottom_up_axis, 'Bottom Up')
